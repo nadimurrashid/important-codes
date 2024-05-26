82eac80e12b1c56e6b510d388f08cd34a2282911
nadimurrashid <48909913+nadimurrashid@users.noreply.github.com>
2024-05-26 18:12:24 +0600

    Update new_img_23.py

--- new_img_23.py
+++ new_img_23.py
@@ -4,20 +4,15 @@ unchanged_file = r"C:\Users\Sazid\Downloads\secondary\Tax Secondary\Secondary_CA
 changed_file = r"C:\Users\Sazid\Downloads\secondary\Tax Secondary\test_changed\random.pdf"
 import aspose
 
-# Input PDF files (list of file paths)
 input_pdf_files = [unchanged_file]
 
-# Output PDF file path
 output_pdf_path = changed_file
 
-# Create a new Document object
 combined_document = Document()
 
-# Iterate through the input PDF files and add their pages to the combined document
 for pdf_file in input_pdf_files:
     pdf_document = Document(pdf_file)
     for page in pdf_document.pages:
         combined_document.pages.add(page)
 
-# Save the combined PDF as a single borderless page
 combined_document.save(output_pdf_path)
