599f9985d995cf0666b1922b836ee7b3d249f1ac
nadimurrashid <48909913+nadimurrashid@users.noreply.github.com>
2024-05-26 18:13:12 +0600

    Update test_image_resizer.py

--- test_image_resizer.py
+++ test_image_resizer.py
@@ -7,9 +7,6 @@ path_to_save = r"C:\Users\Sazid\Desktop\final_csv_result\out_test1.png"
 img = Image.open(file_location)
 rgb_data = np.array(img)
 
-# modify your image
 
 rgb_data = Image.fromarray(rgb_data)
 rgb_data.save(path_to_save, format="PNG")
-# you can also specify dpi, quality and etc.
-# for example rgb_data.save(path_to_save, format="PNG", dpi=(300,300))
