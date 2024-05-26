f83762ac700a8732f916da22044774a3769b0412
nadimurrashid <48909913+nadimurrashid@users.noreply.github.com>
2024-05-26 18:20:26 +0600

    Update image_search.py

--- image_search.py
+++ image_search.py
@@ -5,11 +5,9 @@ import numpy as np
 ImageGrab.grab().save("sample.png")
 
 def find_image_in_image(image, template):
-    # Load images
     img = cv2.imread(image)
     templ = cv2.imread(template)
 
-    # Find template in image
     res = cv2.matchTemplate(img, templ, cv2.TM_CCOEFF_NORMED)
     threshold = 0.8
     loc = np.where(res >= threshold)
